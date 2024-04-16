e6b3fe6464ceb42428a56f90a56e875580ee25f8
AlexBalogh1933 <stickmanaloox@gmail.com>
2024-04-16 08:56:48 -0400

    Add project files.

---
+++ MobileAppFinal/MobileAppFinal/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MobileAppFinal.Models;
+using SQLite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppFinal
+{
+    public partial class App : Application
+    {
+        public static SQLiteConnection Database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileAppFinal/MobileAppFinal/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace MobileAppFinal
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
